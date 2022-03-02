7b31ae428467b32a9bffb3d7e303561fee4e3537
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-03-02 13:03:47 -0500

    commit tree cffdf1fab001c4c83c923e82e3e9928979c1d351

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+ function submitData(name, email) {
+     return fetch("http://localhost:3000/users",{
+         method: "post",
+         headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+         },
+         body: JSON.stringify({
+            name,
+            email,
+          }),
+     })
+     .then(response => response.json())
+     .then (user => document.body.append(user.id))
+     .catch(error => document.body.append('Unauthorized Access'))   
+ }
+submitData()
